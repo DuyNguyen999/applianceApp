f5d34c5c71a9b3e52ff2d590f338bbb97fd4cba1
DuyN <ngocduynguyen.aug@gmail.com>
2021-04-09 01:16:31 +1000

    edited test description

--- spec/appliance_spec.rb
+++ spec/appliance_spec.rb
@@ -3,35 +3,17 @@ require "./Appliance"
 RSpec.describe "Appliance" do
     
     it "should respond to name" do
-        expect(Appliance.new "TV", "Sony", "Liv").to respond_to (:name)    
+        expect(Appliance.new "TV", "Sony", "Living Room").to respond_to (:name)    
     end
 
-    it "should be assign" do
-        appliance = Appliance.new "TV", "Sony", "Liv"
+    it "should assign attribute to the object when initializing" do
+        appliance = Appliance.new "TV", "Sony", "Living Room"
         expect(appliance.name).to eq "TV"
     end
    
-    it "create new object" do
-        expect {Appliance.new "TV", "Sony", "Liv"}.to change {Appliance::APPLIANCES.length}.by 1
-    end
-
-
-    # it "should be respond to save" do
-    #     expect(Appliance.delete).to respond_to APPLIANCES
-    # end
+    it "create new object add to the array" do
+        expect {Appliance.new "TV", "Sony", "Living Room"}.to change {Appliance::APPLIANCES.length}.by 1
+    end  
 end
 
-# RSpec.describe "testing dmm" do
-#     it "can add" do
-#         actual_result = 1 + 1
-#         expected_result = 3
-#         expect(actual_result).not_to eq expected_result
-#     end
-# end
-# RSpec.describe "testing dmm" do
-#     it "can add" do
-#         actual_result = 1 + 1
-#         expected_result = 2
-#         expect(actual_result).to eq expected_result
-#     end
-# end
+
