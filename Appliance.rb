0577cc862ebd929b501a66d75889ed5b84f1932a
DuyN <ngocduynguyen.aug@gmail.com>
2021-04-11 03:53:41 +1000

    edit command lines

--- Appliance.rb
+++ Appliance.rb
@@ -2,25 +2,27 @@ class Appliance
     attr_accessor :name, :made, :room
 
     APPLIANCES = []
-
+    # create new object
     def initialize (name, made, room)
         @name = name
         @made = made
         @room = room
-        APPLIANCES << self
+        APPLIANCES << self # add to the array
     end
-  
+    #find method
     def self.find(keyword)
         APPLIANCES.each do |ele|
             return puts "We have one in the house" if ele.name == keyword                       
         end        
         puts "No, we need to buy one. An alien may steal it !!!"
     end
+    #modify method
     def self.modify(search_input, new_input_name, new_input_brand, new_input_room)
         APPLIANCES.each do |ele|            
             return ele.name = new_input_name, ele.made = new_input_brand, ele.room = new_input_room if ele.name == search_input             
         end         
     end
+    #delete method
     def self.delete(keyword)
         APPLIANCES.each do |ele|
             if ele.name == keyword
@@ -30,6 +32,7 @@ class Appliance
             end
         end
     end    
+    #list method
     def self.list
         temp = []
         APPLIANCES.each do |x|
@@ -41,6 +44,7 @@ class Appliance
             
         end
     end
+    #to string method
     def to_s
         [            
             "NAME: ","#{@name}",
--- main.rb
+++ main.rb
@@ -4,13 +4,14 @@ require "tty-progressbar"
 require "tty-font"
 require "colorize"
 
-
-# This method will iterate over each line individually from the .txt # file and print it to the console
+# This is the app intro
 font = TTY::Font.new(:starwars)
 puts font.write("Welcome to".center(30)).cyan
+# iterate over each line individually from the .txt file and print it to the console
 puts File.read(File.open("./ascii-art.txt")).yellow
 puts ""
 
+# progress bar
 loading_percent = TTY::ProgressBar.new("Loading [:percent]", total: 25)
 25.times do
     sleep(0.05)
@@ -19,13 +20,17 @@ loading_percent = TTY::ProgressBar.new("Loading [:percent]", total: 25)
 
 prompt = TTY::Prompt.new
 puts ""
+# asking users for name
 user_name = prompt.ask("What is your name?", default: "your name...").chomp
+# gretting
 puts "Hello #{user_name} !!".center(100)
 puts "You have 7 command options: Add, List, Modify, Delete, Find, Help, Quit".center(100)
 begin   
+    # taking user command input
     command = ['Add', 'List', 'Modify', 'Delete', 'Find', 'Help', 'Quit']
     input = prompt.select("Choose your command: ", command).chomp
 
+    # create new item
     if input == "Add"
         puts "\nName of the appliance ".yellow
         name = gets.chomp
@@ -38,10 +43,12 @@ begin
         
         appliances = Appliance.new name, made, room
     end
+    # show all items in the array
     if input == "List"
         puts "\nInformation of all the appliances"
         puts Appliance.list             
     end
+    # edit item detail
     if input == "Modify"
         puts "\nName of the appliance that needs to modified "
         search_input = gets.chomp
@@ -55,20 +62,23 @@ begin
         
         Appliance.modify search_input, new_name, new_brand, new_room
     end
+    # delete item 
     if input == "Delete"
         puts "Please enter the item you need to delete: "
         delete_keyword = gets.chomp
         Appliance.delete delete_keyword
-        # case sensitive 
     end
+    # find item
     if input == "Find"
         puts "Please enter the item you need to find: "
         find_keyword = gets.chomp
         Appliance.find find_keyword
     end
+    # loading help document
     if input == "Help"
         puts File.read(File.open("./help.txt"))
     end
+# exception handling
 rescue
     puts "Error occured"
-end until ['Quit','q'].include? input
+end until ['Quit','q'].include? input # exit the program
